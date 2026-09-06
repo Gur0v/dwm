5b919582c697212976b9b6f9b8890ef478848909
Gurov <gurov@disroot.org>
2026-09-06 16:33:58 +0300

    fix border compositing, gappx 2px -> 4px

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 2;        /* gap pixel between windows */
+static const unsigned int gappx     = 4;        /* gap pixel between windows */
 static const int snap               = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
